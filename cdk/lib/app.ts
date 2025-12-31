30fe1761f57553b0b7f31e720fd97c0970fdb5a2
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2025-12-30 18:39:31 -0800

    feat(gha): add cdk deploy from github oidc (#125)

--- cdk/lib/app.ts
+++ cdk/lib/app.ts
@@ -2,11 +2,21 @@
 import * as cdk from "aws-cdk-lib";
 
 import {ConnectionsStack} from "./connections";
+import {GitHubOidcStack} from "./github-oidc";
 
 const app = new cdk.App();
 
 const REGION = "us-east-1";
 const DOMAIN_NAME = "connections.layertwo.dev";
+
+new GitHubOidcStack(app, "GitHubOidcStack", {
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: REGION,
+    },
+    githubOrg: "layertwo",
+    githubRepo: "connections",
+});
 
 new ConnectionsStack(app, "ConnectionsStack", {
     env: {
