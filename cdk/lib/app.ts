d30276b47eeb6a21782d194009b1d790b22b6206
Lucas Messenger <1335960+layertwo@users.noreply.github.com>
2025-12-30 21:57:05 -0500

    fix: set account id

--- cdk/lib/app.ts
+++ cdk/lib/app.ts
@@ -6,23 +6,23 @@ import {GitHubOidcStack} from "./github-oidc";
 
 const app = new cdk.App();
 
+const AWS_ACCOUNT = "578999165660";
 const REGION = "us-east-1";
 const DOMAIN_NAME = "connections.layertwo.dev";
 
+const env = {
+    account: AWS_ACCOUNT,
+    region: REGION,
+};
+
 new GitHubOidcStack(app, "GitHubOidcStack", {
-    env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: REGION,
-    },
+    env,
     githubOrg: "layertwo",
     githubRepo: "connections",
 });
 
 new ConnectionsStack(app, "ConnectionsStack", {
-    env: {
-        account: process.env.CDK_DEFAULT_ACCOUNT,
-        region: REGION,
-    },
+    env,
     domainName: DOMAIN_NAME,
 });
 
